8d4495737da704fb4eebb6ca5ce0791ddc9a15df
Rens Favre <rensfavre@gmail.com>
2023-02-10 14:53:06 +0100

    time fix

--- main.go
+++ main.go
@@ -8,16 +8,15 @@ import (
 func main() {
 	hour := time.Now().Hour()
 	fmt.Println("Welkom bij Fonteyn Vakantieparken!")
-	if hour == 7 {
-		fmt.Println("goedemorgen!")
-	}
-	if hour == 13 {
-		fmt.Println("goedemidag!")
-	}
-	if hour == 18 {
+	if hour > 23 {
+		fmt.Println("Sorry, de parkeerplaats is ’s nachts gesloten")
+	} else if hour > 18 {
 		fmt.Println("goedenavond!")
-	}
-	if hour == 23 {
+	} else if hour > 13 {
+		fmt.Println("goedemidag!")
+	} else if hour > 7 {
+		fmt.Println("goedemorgen!")
+	} else {
 		fmt.Println("Sorry, de parkeerplaats is ’s nachts gesloten")
 	}
 }
